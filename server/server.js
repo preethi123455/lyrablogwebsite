18d04a4c8ca9981f1f7645924105c518494fc241
preethi123455 <preethiusha007@gmail.com>
2025-12-26 19:33:14 +0530

    patch

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ app.use(
   cors({
     origin: "https://lyrablogwebsite-frontend.onrender.com",
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS","PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
