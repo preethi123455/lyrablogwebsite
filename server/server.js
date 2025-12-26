b9f1df3cf9551f31bace3955cd1eb612c9f1d7b4
preethi123455 <preethiusha007@gmail.com>
2025-12-26 18:41:27 +0530

    frontend cors

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ import cors from "cors";
 const app = express();
 app.use(
   cors({
-    origin: ["http://localhost:3000", "http://localhost:3001"],
+    origin: ["http://localhost:3000", "http://localhost:3001","https://lyrablogwebsite-frontend.onrender.com/"],
     credentials: true,
   })
 );
