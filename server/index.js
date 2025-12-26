028806948f6caf85c3b67341a8fe1edb246b3e5b
preethi123455 <preethiusha007@gmail.com>
2025-12-26 19:47:15 +0530

    index -1

--- server/index.js
+++ server/index.js
@@ -7,7 +7,10 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: ["http://localhost:3000",
+      "https://lyrablogwebsite-frontend.onrender.com",
+
+    ],
     methods: ["GET", "POST"],
   },
   transports: ["websocket"],   
